e76090bdd727669fd28d9d06350911cd79da1ce9
Myown <htons.ntohs@gmail.com>
2015-01-04 17:45:40 +0900

    Create a wrapper for words cleaning and loading, server functions.

---
+++ startServer.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+import os
+
+CURRENT_DIR = os.getcwd()
+RESOURCE_DIR = "%s/resources" % CURRENT_DIR
+WORD_BANK = "%s/words.txt" % RESOURCE_DIR
+TMP_WORD_BANK = "%s/words.tmp.txt" % RESOURCE_DIR
+
+def clean_word_bank():
+    lines = open(WORD_BANK, "r").readlines()
+    targf = open(TMP_WORD_BANK, "w")
+    delchars = ''.join(c for c in map(chr, range(256)) if not c.isalnum())
+
+    for line in lines:
+        scrunched = line.lower().strip().translate(None, delchars)
+        print(scrunched, file=targf)
+
+    targf.close()
+
+clean_word_bank()
