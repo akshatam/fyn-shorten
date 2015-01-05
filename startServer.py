c303e069deab7f6feed6f1dfaf3a3bf61ab61b02
Amar Akshat <akshat@akshatam.in>
2015-01-05 17:47:22 +0900

    starServer:single point trigger for server functions.

--- startServer.py
+++ startServer.py
@@ -1,10 +1,13 @@
 from __future__ import print_function
 import os
+import subprocess
+import sys
 
 CURRENT_DIR = os.getcwd()
 RESOURCE_DIR = "%s/resources" % CURRENT_DIR
 WORD_BANK = "%s/words.txt" % RESOURCE_DIR
 TMP_WORD_BANK = "%s/words.tmp.txt" % RESOURCE_DIR
+PROJECT_DIR = "%s/tinyurl" % CURRENT_DIR
 
 def clean_word_bank():
     lines = open(WORD_BANK, "r").readlines()
@@ -17,6 +20,24 @@ def clean_word_bank():
 
     targf.close()
     os.environ["TMP_WORD_BANK_FILE"] = TMP_WORD_BANK
+    
+  
+def load_word_bank():
+    parent_dir = os.getcwd()
+    os.chdir(PROJECT_DIR)
+    subprocess.call(["python", "manage.py", "wordbank"])
+    os.chdir(parent_dir)
+  
+def manage_app():
+    parent_dir = os.getcwd()
+    os.chdir(PROJECT_DIR)
+    subprocess.call(["python", "manage.py", "migrate"])
+    subprocess.call(["python", "manage.py", "runserver", "0.0.0.0:8000"])
+    os.chdir(parent_dir)
+    
 
-
-clean_word_bank()
+if "--reload" in sys.argv or "-r" in sys.argv:
+    clean_word_bank()
+    load_word_bank()
+    
+manage_app()
